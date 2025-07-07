d4a2aaedb5f2bf7680067b6edd76a35cbe882f0d
soundar <soundar4121@gmail.com>
2025-07-08 00:05:56 +0530

    Created Frontend using react (Login page)

---
+++ frontend/frontend/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import LoginPage from './components/LoginPage';
+import './App.css';
+
+function App() {
+  return (
+    <LoginPage/>
+  );
+}
+
+export default App;
---
+++ frontend/frontend/src/components/LoginPage.js
@@ -0,0 +1,42 @@
+
+import React, { useState } from 'react';
+
+
+function LoginPage() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    console.log('Email:', email);
+    console.log('Password:', password);
+    alert(`Logged in as ${email}`);
+  };
+
+  return (
+    <div className="login-container">
+      <h2>Login</h2>
+      <form onSubmit={handleLogin}>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default LoginPage;
